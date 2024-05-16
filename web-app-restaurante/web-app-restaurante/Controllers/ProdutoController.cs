294adee33a58b8f16da58b1bbb8ea12296c54f7a
xavier7132 <xavier7132@gmail.com>
2024-05-16 17:51:08 -0300

    web api com banco sqlite

---
+++ web-app-restaurante/web-app-restaurante/Controllers/ProdutoController.cs
@@ -0,0 +1,84 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using System.Data;
+using web_app_restaurante.Entidades;
+
+namespace web_app_restaurante.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly string? _connectionString;
+
+        public ProdutoController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private IDbConnection OpenConnection()
+        {
+            IDbConnection dbConnection = new SqliteConnection(_connectionString);
+            dbConnection.Open();
+            return dbConnection;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            using IDbConnection dbConnection = OpenConnection();
+            var result = await dbConnection.QueryAsync<Produto>("select id, nome, descricao, imagemUrl from produto;");
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            using IDbConnection dbConnection = OpenConnection();
+
+            var produto = await dbConnection.QueryAsync<Produto>("select id, nome, descricao, imagemUrl from produto where id = @id;", new { id });
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Produto produto)
+        {
+            using IDbConnection dbConnection = OpenConnection();
+            
+            dbConnection.Execute("insert into Produto(nome, descricao, imagemUrl) values(@Nome, @Descricao, @ImagemUrl)", produto);
+            return Created();
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Produto produto)
+        {
+
+            using IDbConnection dbConnection = OpenConnection();
+
+            // Atualiza o produto
+            var query = @"UPDATE Produto SET 
+                              Nome = @Nome,
+                              Descricao = @Descricao,
+                              ImagemUrl = @ImagemUrl
+                              WHERE Id = @Id";
+
+            dbConnection.Execute(query, produto);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            using IDbConnection dbConnection = OpenConnection();
+
+            var produto = await dbConnection.QueryAsync<Produto>("delete from produto where id = @id;", new {  id });
+            return Ok();
+        }
+    }
+}
